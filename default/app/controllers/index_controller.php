0c88fa1dcb8395642109306f849284430dd1841d
jaimeirazabal1 <jaimeirazabal1@gmail.com>
2015-10-26 11:22:24 -0430

    template de administracion con bootstrap incluido con kumbiaphp

---
+++ default/app/controllers/index_controller.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Controller por defecto si no se usa el routes
+ * 
+ */
+class IndexController extends AppController
+{
+
+    public function index()
+    {
+        
+    }
+    public function blank(){
+
+    }
+    public function buttons(){
+
+    }
+    public function flot(){
+
+    }
+    public function forms(){
+
+    }
+    public function grid(){
+
+    }
+    public function icons(){
+
+    }
+    public function login(){
+        View::template('login');
+    }
+    public function morris(){
+
+    }
+    public function notifications(){
+
+    }
+    public function panels_wells(){
+
+    }
+    public function tables(){
+
+    }
+    public function typography(){
+    	
+    }
+}
